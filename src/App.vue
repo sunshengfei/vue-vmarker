6e12f927fd406099b30442b4a43d59a4cc7d827a
freddon <fabyuxuan@163.com>
2019-10-23 23:32:15 +0800

    add test branch

---
+++ src/App.vue
@@ -0,0 +1,92 @@
+<template>
+  <div id="app">
+      <AiPanel ref="aiPanel-editor"
+       class="ai-observer" :ratio="ratio" v-bind:uniqueKey="1" @vmarker:onSelect="selectOne" @vmarker:onUpdated="onUpdated" @vmarker:onDrawOne="_drawOne"
+              @vmarker:onReady="onAiPanelReady" @vmarker:onImageLoad="onImageLoad" v-bind:readOnly="false" v-bind:imgUrl="currentImage()"></AiPanel>
+  </div>
+</template>
+
+<script>
+import { AIMarker as AiPanel } from "vue-picture-bd-marker";
+export default {
+  name: "app",
+  data() {
+    return {
+      ratio: 1.3333,
+      photoWH: {
+        sourceWH: {
+          souW: 0,
+          souH: 0
+        },
+        newWH: {
+          newW: 0,
+          newH: 0
+        }
+      }
+    };
+  },
+  components: { AiPanel },
+  mounted() {
+    window.thiz = this;
+  },
+  methods: {
+    currentImage() {
+      return "https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=1139702265,431383255&fm=26&gp=0.jpg";
+    },
+    onAiPanelReady() {},
+    onImageLoad() {
+      this.ratio = 1.333333;
+    }, // 松手触发
+    onUpdated(data, m) {
+      console.log("🦁onUpdated🦁 data=", data);
+      this.tagList = data;
+      this.isDisabled = false;
+    },
+    // 单选触发
+    selectOne(data, uKey) {
+      console.log("🤚selectOne data=", data);
+    },
+    // 单个标签方法
+    _drawOne(data) {},
+    // 打标签
+    _doTag(tagStr) {
+      let mirror = this.$refs["aiPanel-editor"];
+      // mirror.dispatchEvent("setTag", tagStr);
+      mirror.getMarker().setTag({
+        tagName: "小蜜蜂",
+        tag: "0x0001"
+      });
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1,
+h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
